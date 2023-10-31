b8182e2cb72a64a8e851c5b4583cfb9840f09ea0
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-10-31 22:00:19 +0100

    createTokens method in Player class

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,19 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = [this.createTokens(21)];
+    }
+/**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an array of new token objects
+  */
+    createTokens (num) {
+        const token = [];
+        for(let i = 0; i < num ; i++){
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+        return tokens;
     }
 }
