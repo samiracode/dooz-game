adee115c369818fe47b07255713235f99e0cd629
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-11-01 14:07:37 +0100

    Game object

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,20 @@
-class Game {}
+class Game {
+    constructor (board, players, ready) {
+        this.board = new Board ();
+        this.players = [this.createPlayers()];
+        this.ready = false;
+    }
+
+    /** 
+    * Creates two player objects
+    * @return  {Array}    An array of two Player objects.
+    */
+    createPlayers() {
+        const players = []
+        playerOne = new Player()
+        playerTwo = new Player()
+        players.push(playerOne)
+        players.push(playerTwo)
+        return players;
+    }
+}
