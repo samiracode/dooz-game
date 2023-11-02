015be200d203d9b453a4c099270d28534794f780
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-11-02 16:02:41 +0100

    updateGameState method done

--- js/Game.js
+++ js/Game.js
@@ -34,126 +34,160 @@ class Game {
         this.activePlayer.activeToken.drawHTMLToken();
         this.ready = true;
     }
-
+	
+	
     /**
-    * Branches code, depending on what key player presses
-    * @param   {Object}    e - Keydown event object
-    */
-    handleKeydown (e) {
-        if(this.ready) {
-            if(e.key === "ArrowLeft") {
-                // move left
-               this.activePlayer.activeToken.moveLeft();
-            } else if (e.key === "ArrowRight"){
-                // move right
-                this.activePlayer.activeToken.moveRight(this.board.columns)
+	 * Branches code, depending on what key player presses
+	 * @param	{Object}	e - Keydown event object
+	 */
+	handleKeydown(e) {
+        if (this.ready) {
+            if (e.key === "ArrowLeft") {
+                this.activePlayer.activeToken.moveLeft();
+            } else if (e.key === "ArrowRight") {
+                this.activePlayer.activeToken.moveRight(this.board.columns);
             } else if (e.key === "ArrowDown") {
-                // play token
-                this.playToken()
+                this.playToken();
             }
         }
     }
+    
 
-
-    playToken () {
+    /**
+     * Finds Space object to drop Token into, drops Token
+     */
+    playToken(){
         let spaces = this.board.spaces;
         let activeToken = this.activePlayer.activeToken;
         let targetColumn = spaces[activeToken.columnLocation];
         let targetSpace = null;
 
-        for (let space of targetColumn){
-            if (space.token === null ) {
-                targetSpace = space;
-            }
+		for (let space of targetColumn) {
+			if (space.token === null) {
+				targetSpace = space;
+			}
         }
 
         if (targetSpace !== null) {
-            game.ready = false; 
-            activeToken.drop(targetSpace);
-        }
+			const game = this;
+            game.ready = false;
+
+    		activeToken.drop(targetSpace, function(){
+                game.updateGameState(activeToken, targetSpace);           
+    		});  
+        }              
     }
 
+
     /** 
-    * Checks if there a winner on the board after each token drop.
-    * @param   {Object}    Targeted space for dropped token.
-    * @return  {boolean}   Boolean value indicating whether the game has been won (true) or not (false)
+     * Updates game state after token is dropped. 
+     * @param   {Object}    token - The token that's being dropped.
+     * @param   {Object}    target - Targeted space for dropped token.
+     */
+    updateGameState(token, target) {
+		target.mark(token);
+
+        if (!this.checkForWin(target)) {
+            console.log('no win');
+			this.switchPlayers();
+            
+            if (this.activePlayer.checkTokens()) {
+                this.activePlayer.activeToken.drawHTMLToken();
+                this.ready = true;
+            } else {
+                this.gameOver('No more tokens');
+            }
+        } else {
+			console.log('win');
+            this.gameOver(`${target.owner.name} wins!`)
+        }			
+    }
+
+    
+    /** 
+     * Checks if there a winner on the board after each token drop.
+     * @param   {Object}    target - Targeted space for dropped token.
+     * @return  {boolean}   Boolean value indicating whether the game has been won (true) or not (false)
     */
     checkForWin(target){
-        const owner = target.token.owner;
-        let win = false;
-
-        // Vertical
-        for (let x = 0; x < this.board.columns; x++) {
-            for (let y = 0; y < this.board.rows -3; y++) {
-                if (this.board.spaces[x][y].owner === owner &&
-                    this.board.spaces[x][y+1].owner === owner &&
-                    this.board.spaces[x][y+2].owner === owner &&
-                    this.board.spaces[x][y+3].owner === owner) {
-
-                        win = true;
-                }   
+    	const owner = target.token.owner;
+    	let win = false;
+		console.log('checkforwin called');
+    	// vertical
+    	for (let x = 0; x < this.board.columns; x++ ){
+            for (let y = 0; y < this.board.rows - 3; y++){
+				console.log(x,y);
+				console.log(y+1);
+				console.log(y+2);
+				console.log(y+3);
+                if (this.board.spaces[x][y].owner === owner && 
+    				this.board.spaces[x][y+1].owner === owner && 
+    				this.board.spaces[x][y+2].owner === owner && 
+    				this.board.spaces[x][y+3].owner === owner) {
+                    	win = true;
+						console.log(win);
+                }           
             }
         }
-  
-        // Horizontal 
-        for (let x = 0; x < this.board.columns - 3; x++) {
-            for (let y = 0; y < this.board.rows; y++) {
-                if (this.board.spaces[x][y].owner === owner &&
-                    this.board.spaces[x+1][y].owner === owner &&
-                    this.board.spaces[x+2][y].owner === owner &&
-                    this.board.spaces[x+3][y].owner === owner) {
-
-                        win = true;
-                }   
+	
+    	// horizontal
+    	for (let x = 0; x < this.board.columns - 3; x++ ){
+            for (let y = 0; y < this.board.rows; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+    				this.board.spaces[x+1][y].owner === owner && 
+    				this.board.spaces[x+2][y].owner === owner && 
+    				this.board.spaces[x+3][y].owner === owner) {
+                    	win = true;
+                }           
             }
         }
-        
-        // Diagonal towards down
-        for (let x = 3; x < this.board.columns; x++) {
-            for (let y = 0; y < this.board.rows - 3; y++) {
-                if (this.board.spaces[x][y].owner === owner &&
-                    this.board.spaces[x-1][y+1].owner === owner &&
-                    this.board.spaces[x-2][y+2].owner === owner &&
-                    this.board.spaces[x-3][y+3].owner === owner) {
-
-                        win = true;
-                }   
+		
+    	// diagonal
+    	for (let x = 3; x < this.board.columns; x++ ){
+            for (let y = 0; y < this.board.rows - 3; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+    				this.board.spaces[x-1][y+1].owner === owner && 
+    				this.board.spaces[x-2][y+2].owner === owner && 
+    				this.board.spaces[x-3][y+3].owner === owner) {
+                    	win = true;
+                }           
             }
         }
-
-        // Diagonal towards up
-        for (let x = 3; x < this.board.columns; x++) {
-            for (let y = 3; y < this.board.rows; y++) {
-                if (this.board.spaces[x][y].owner === owner &&
-                    this.board.spaces[x-1][y-1].owner === owner &&
-                    this.board.spaces[x-2][y-2].owner === owner &&
-                    this.board.spaces[x-3][y-3].owner === owner) {
-
-                        win = true;
-                }   
+	
+    	// diagonal
+    	for (let x = 3; x < this.board.columns; x++ ){
+            for (let y = 3; y < this.board.rows; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+    				this.board.spaces[x-1][y-1].owner === owner && 
+    				this.board.spaces[x-2][y-2].owner === owner && 
+    				this.board.spaces[x-3][y-3].owner === owner) {
+                    	win = true;
+                }           
             }
         }
-
-        return win;
+	
+    	return win;
     }
 
-
-    // Switches active player
-    switchPlayer () {
-        for (let player of this.players) {
-          player.active = player.active === true ? false : true;
-        }
-    }
 
     /** 
-    * Displays game over message.
-    * @param {string} message - Game over message.      
-    */
-    gameOver (message) {
-        document.getElementById('game-over').style.display = 'block';
-        document.getElementById('game-over').textContent = message;
+     * Switches active player. 
+     */
+	switchPlayers() {
+		for (let player of this.players) {
+			player.active = player.active === true ? false : true;
+		}
     }
     
+
+    /** 
+     * Displays winner info.
+     * @param   {String}    message - Game over message.      
+     */
+    gameOver(message) {
+		document.getElementById('game-over').style.display = 'block';
+        document.getElementById('game-over').textContent = message;
+    }
 
 
 
