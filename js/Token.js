7c68b4167871b49b8faf9534bc547ff6393941f0
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-11-02 09:29:09 +0100

    drop method & jQuery animation done

--- js/Token.js
+++ js/Token.js
@@ -45,4 +45,19 @@ class Token {
             this.columnLocation += 1;
         }
     }
+
+
+    /** 
+    * Drops html token into targeted board space.
+    * @param   {Object}   target - Targeted space for dropped token.
+    * @param   {function} reset  - The reset function to call after the drop animation has completed.
+    */
+    drop (target, reset) {
+        this.dropped = true;
+        
+        $(this.htmlToken).animate ({
+            top : (target.y * target.diameter )
+            
+        }, 750, 'easeOutBounce', reset);
+    }
 }
